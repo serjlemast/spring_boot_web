fbf9976a3447d4cbc5e7fe60e14101838ce9e34f
Dimitrii Maksymov <dimitriimaksymov@Dimitriis-MacBook-Air.local>
2025-12-19 15:57:55 +0200

    add modules

---
+++ web/rest_annotation_base_style/src/main/java/org/example/rest_annotation_base_style/RestAnnotationBaseStyleApplication.java
@@ -0,0 +1,13 @@
+package org.example.rest_annotation_base_style;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestAnnotationBaseStyleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestAnnotationBaseStyleApplication.class, args);
+    }
+
+}
